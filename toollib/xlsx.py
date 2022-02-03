6c97527ea87b084f7d62d809e4dbb8d2a2119fb8
atpuxiner <atpuxiner@163.com>
2022-02-04 00:13:47 +0800

    add@2022.02.04

--- toollib/xlsx.py
+++ toollib/xlsx.py
@@ -14,14 +14,15 @@ except ImportError:
     raise
 
 __all__ = [
-    "inserts",
-    "rows_value",
-    "cols_value",
+    "ws_inserts",
+    "ws_rows_value",
+    "ws_cols_value",
+    "ws_styles",
 ]
 
 
-def inserts(ws: Worksheet, values: t.List[list], index: int = None,
-            mode: str = "r", is_new: bool = True):
+def ws_inserts(ws: Worksheet, values: t.List[list], index: int = None,
+               mode: str = "r", is_new: bool = True):
     """
     插入数据
     :param ws: Worksheet实例（openpyxl库）
@@ -60,7 +61,7 @@ def inserts(ws: Worksheet, values: t.List[list], index: int = None,
             ws.append(item)
 
 
-def rows_value(ws: Worksheet):
+def ws_rows_value(ws: Worksheet):
     """
     所有行的值
     :param ws: Worksheet实例（openpyxl库）
@@ -70,7 +71,7 @@ def rows_value(ws: Worksheet):
         yield [r.value for r in item]
 
 
-def cols_value(ws: Worksheet):
+def ws_cols_value(ws: Worksheet):
     """
     所有列的值
     :param ws: Worksheet实例（openpyxl库）
@@ -78,3 +79,102 @@ def cols_value(ws: Worksheet):
     """
     for item in ws.columns:
         yield [c.value for c in item]
+
+
+def ws_styles(
+        ws,
+        styles: dict,
+        by_icells: str = None,
+        by_cells: t.Union[str, t.List[str]] = None,
+        by_rows: t.Union[int, t.List[int]] = None,
+        by_cols: t.Union[str, t.List[str]] = None,
+        exclude_icell: t.Union[t.List[t.Tuple[int, int]], t.Tuple[int, int]] = None,
+):
+    """
+    修改样式
+    :param ws: Worksheet实例（openpyxl库）
+    :param styles: 样式。以字典形式传入
+    :param by_icells: 按单元格索引。eg: '1:2,1:2' >>> 左行：1至2行，右列：1至2列
+    :param by_cells: 按单元格。eg: 'A1' or ['A1', 'B1', 'C1']
+    :param by_rows: 按行。eg: 1 or [1, 2, 3]
+    :param by_cols: 按列。eg: 'A' or ['A', 'B', 'C']
+    :param exclude_icell:
+    :return:
+    """
+    if by_icells:
+        by_icells = by_icells.strip()
+        if by_icells in ['0', '0:0']:
+            rows, cols = (1, ws.max_row), (1, ws.max_column)
+        else:
+            if by_icells.find(',') == -1:
+                by_icells += ','
+            rows, cols = by_icells.split(',')
+            if not rows:
+                rows = (1, ws.max_row)
+            else:
+                if rows.find(':') == -1:
+                    _row = int(rows)
+                    rows = (_row, _row)
+                else:
+                    _lrow, _rrow = rows.split(':')
+                    rows = (int(_lrow), int(_rrow))
+            if not cols:
+                cols = (1, ws.max_column)
+            else:
+                if cols.find(':') == -1:
+                    _col = int(cols)
+                    cols = (_col, _col)
+                else:
+                    _lcol, _rcol = cols.split(':')
+                    cols = (int(_lcol), int(_rcol))
+        for r in range(rows[0], rows[1]+1):
+            for c in range(cols[0], cols[1]+1):
+                _curr_icell = (r, c)
+                if isinstance(exclude_icell, tuple):
+                    if _curr_icell == exclude_icell:
+                        continue
+                elif isinstance(exclude_icell, list):
+                    if exclude_icell.count(_curr_icell) > 0:
+                        continue
+                ws_scope = ws.cell(r, c)
+                for stl, stl_value in styles.items():
+                    try:
+                        getattr(ws_scope, stl)
+                    except AttributeError:
+                        raise
+                    setattr(ws_scope, stl, stl_value)
+    elif by_cells:
+        if isinstance(by_cells, str):
+            by_cells = [by_cells]
+        for cell in by_cells:
+            ws_scope = ws[cell]
+            for stl, stl_value in styles.items():
+                try:
+                    getattr(ws_scope, stl)
+                except AttributeError:
+                    raise
+                setattr(ws_scope, stl, stl_value)
+    elif by_rows:
+        if isinstance(by_rows, int):
+            by_rows = [by_rows]
+        for row in by_rows:
+            ws_scope = ws.row_dimensions[row]
+            for stl, stl_value in styles.items():
+                try:
+                    getattr(ws_scope, stl)
+                except AttributeError:
+                    raise
+                setattr(ws_scope, stl, stl_value)
+    elif by_cols:
+        if isinstance(by_cols, str):
+            by_cols = [by_cols]
+        for col in by_cols:
+            ws_scope = ws.column_dimensions[col]
+            for stl, stl_value in styles.items():
+                try:
+                    getattr(ws_scope, stl)
+                except AttributeError:
+                    raise
+                setattr(ws_scope, stl, stl_value)
+    else:
+        raise ValueError("'by_icells' |'by_cells' |'by_rows' |'by_cols' is required")
