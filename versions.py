e4da5b7cc82f1e1ba33be12c9ab11e42bf99d9ba
atpuxiner <atpuxiner@163.com>
2022-02-08 20:51:34 +0800

    add@2022.02.08

--- versions.py
+++ versions.py
@@ -10,6 +10,7 @@
 
 class Versions(object):
     ALL = [
+        ('2022.02.08', 'Add. eg: regexp.py, useragent.py'),
         ('2022.02.04', 'Add. eg: xlsx.py'),
         ('2022.01.19', 'Opt.'),
         ('2022.01.18', 'Add. eg: webdriver.ChromeDriver'),
@@ -19,7 +20,7 @@ class Versions(object):
         ('2022.01.10', 'Opt.'),
         ('2022.01.09', 'Add docs.'),
         ('2022.01.07', 'Opt libs. eg: ToolG...'),
-        ('2022.01.02', 'Opt libs. rename...'),
+        ('2022.01.02', 'Opt libs. eg: rename...'),
         ('2021.12.30', 'Add libs. eg: ToolException...'),
         ('2021.12.26', 'Add libs. eg: Utils, SingletonType, G...'),
         ('2021.12.14', 'Add libs. eg: DecoratorUtils, TimeUtils...'),
