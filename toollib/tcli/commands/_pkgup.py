7df6964ecacd745b04e3d1a501e368e47f6b7035
apuxiner <apuxiner@163.com>
2025-12-04 14:37:39 +0800

    opt@pytcli.pkgup

--- toollib/tcli/commands/_pkgup.py
+++ toollib/tcli/commands/_pkgup.py
@@ -26,9 +26,9 @@ class Cmd(BaseCmd):
             name='pkgup',
             desc='包更新',
             optional={self.pkgup: [
-                Arg("-r", "--requirements", default="requirements.txt", type=str,
-                    help='依赖文件(默认requirements.txt)'),
-                Arg("--overwrite", action='store_true', help='是否覆盖（默认不覆盖）')
+                Arg("-r", "--requirements", default="requirements.txt", type=str, help='依赖文件（默认requirements.txt）'),
+                Arg("-s", "--sep", default="==", choices=["==", ">=", "<=", "~="], help='分隔符（默认==）'),
+                Arg("--overwrite", action='store_true', help='是否覆盖（默认不覆盖）'),
             ]}
         )
         return options
@@ -40,41 +40,40 @@ class Cmd(BaseCmd):
             sys.exit(1)
         with open(requirements, "r", encoding="utf-8") as f:
             try:
-                pkgs = []
-                for t in f:
+                pkgs = {}
+                for i, t in enumerate(f):
                     if t := t.strip():
                         if t.startswith("#"):
                             continue
                         if pkg := re.split(r"[<>=~]", t, maxsplit=1)[0].strip():
-                            pkgs.append(pkg)
+                            pkgs[i] = pkg
                 if not pkgs:
-                    sys.stdout.write(f"TIP: {requirements} 未检测到依赖包\n")
+                    sys.stdout.write(f"TIP: `{requirements}` No packages detected\n")
                     sys.exit()
                 subprocess.run(["pip", "install", "-U",
-                                *[p for p in pkgs if p not in ["toollib", "pytcli"]]], check=True)
-                with open(requirements, "r", encoding="utf-8") as f:
-                    text = f.read()
-                for pkg in pkgs:
-                    repl = pkg
-                    if pkg in ["toollib", "pytcli"]:
-                        sys.stdout.write(f"TIP: 请手动更新`{pkg}`\n")
-                    else:
-                        try:
-                            ver = metadata.version(pkg)
-                            repl = f"{pkg}=={ver}"
-                        except metadata.PackageNotFoundError:
-                            pass
-                    text = re.sub(
-                        pattern=rf"^\s*{re.escape(pkg)}(?:\s*$|\s*(?:==|~=|>=|<=|!=|<|>).*)",
-                        repl=repl,
-                        string=text,
-                        flags=re.MULTILINE,
-                    )
+                                *[p for p in pkgs.values() if p not in ["toollib", "pytcli"]]], check=True)
+                new_text = ""
+                f.seek(0)
+                for i, l in enumerate(f):
+                    if pkg := pkgs.get(i):
+                        if pkg in ["toollib", "pytcli"]:
+                            sys.stdout.write(f"TIP: Please update manually `{pkg}`\n")
+                            l = f"{pkg}  # Please update manually"
+                        else:
+                            try:
+                                if "[" in pkg and "]" in pkg:
+                                    ver = metadata.version(pkg[:pkg.index("[")])
+                                else:
+                                    ver = metadata.version(pkg)
+                                l = f"{pkg}{self.parse_args.sep}{ver}"
+                            except metadata.PackageNotFoundError:
+                                pass
+                    new_text += f"{l.strip()}\n"
                 if self.parse_args.overwrite:
                     with open(requirements, "w", encoding="utf-8") as f:
-                        f.write(text)
+                        f.write(new_text)
                 else:
                     with open(requirements + ".up", "w", encoding="utf-8") as f:
-                        f.write(text)
+                        f.write(new_text)
             except Exception:
                 sys.exit(1)
